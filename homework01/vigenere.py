4b9bb261f919e4dcba5b242cd2b73acff2f2c52b
LedviL <samrinis@mail.ru>
2025-10-30 15:27:22 +0300

    done vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -42,8 +42,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     import caesar
-    keyword = keyword * (len(ciphertext) // len(keyword)) + keyword[:len(ciphertext) % len(keyword)]
-    for char, key in zip(ciphertext, keyword):
-        shift = ord(key) - ord('A' if char == char.upper() else 'a')
+    j = 0
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if not char.isalpha():
+            plaintext += char
+            continue
+        if j == len(keyword):
+            j = 0
+        key = keyword[j]
+        if char.isupper() and key.islower():
+            key = key.upper()
+        elif char.islower() and key.isupper():
+            key = key.lower()
+        shift = ord(key) - ord('A' if char.isupper() else 'a')
         plaintext += caesar.decrypt_caesar(char, shift)
+        j += 1
     return plaintext
