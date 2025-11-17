68cd66ec3e885a8794e46dda8da064d3ae6e60ea
LedviL <samrinis@mail.ru>
2025-11-17 18:24:34 +0300

    reformatted scytale.py

--- homework01/scytale.py
+++ homework01/scytale.py
@@ -1,9 +1,10 @@
 def decrypt_scytale(plaintext, n):
+    """decrypts scytale cypher"""
     rows, cols = n, len(plaintext) // n
-    matrix = [[""]*cols for _ in range(rows)]
+    matrix = [[""] * cols for _ in range(rows)]
     c = -1
-    for i in range(len(plaintext)):
+    for i, char in enumerate(plaintext):
         if i % rows == 0:
             c += 1
-        matrix[i % rows][c] = plaintext[i]
+        matrix[i % rows][c] = char
     return "".join(["".join(s) for s in matrix])
