eaa04cf77bedfbb18ea2cbce521cb7b4935bd8b5
LedviL <samrinis@mail.ru>
2025-11-18 12:47:03 +0300

    refactored vigenere.py accroding to the comments

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,21 +11,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = ""
     import caesar
 
-    j = 0
-    for i in range(len(plaintext)):
-        char = plaintext[i]
+
+    keyword_char_index = 0
+    for i, char in enumerate(plaintext):
         if not char.isalpha():
             ciphertext += char
-            j = (j + 1) % len(keyword)
+            keyword_char_index = (keyword_char_index + 1) % len(keyword)
             continue
-        key = keyword[j]
+        key = keyword[keyword_char_index]
         if char.isupper() and key.islower():
             key = key.upper()
         elif char.islower() and key.isupper():
             key = key.lower()
         shift = ord(key) - ord("A" if char.isupper() else "a")
         ciphertext += caesar.encrypt_caesar(char, shift)
-        j = (j + 1) % len(keyword)
+        keyword_char_index = (keyword_char_index + 1) % len(keyword)
 
     return ciphertext
 
@@ -43,19 +43,18 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
     import caesar
 
-    j = 0
-    for i in range(len(ciphertext)):
-        char = ciphertext[i]
+    keyword_char_index = 0
+    for i, char in enumerate(ciphertext):
         if not char.isalpha():
             plaintext += char
-            j = (j + 1) % len(keyword)
+            keyword_char_index = (keyword_char_index + 1) % len(keyword)
             continue
-        key = keyword[j]
+        key = keyword[keyword_char_index]
         if char.isupper() and key.islower():
             key = key.upper()
         elif char.islower() and key.isupper():
             key = key.lower()
         shift = ord(key) - ord("A" if char.isupper() else "a")
         plaintext += caesar.decrypt_caesar(char, shift)
-        j = (j + 1) % len(keyword)
+        keyword_char_index = (keyword_char_index + 1) % len(keyword)
     return plaintext
