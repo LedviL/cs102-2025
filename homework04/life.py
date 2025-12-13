52cf2e74a1f7660113fd5fbda27366a2b4f3e9b9
LedviL <samrinis@mail.ru>
2025-12-14 00:03:41 +0300

    done life.py

--- homework04/life.py
+++ homework04/life.py
@@ -29,46 +29,116 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        """
+        Создание списка клеток.
+
+        Клетка считается живой, если ее значение равно 1, в противном случае клетка
+        считается мертвой, то есть, ее значение равно 0.
+
+        Parameters
+        ----------
+        randomize : bool
+            Если значение истина, то создается матрица, где каждая клетка может
+            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
+
+        Returns
+        ----------
+        out : Grid
+            Матрица клеток размером `cell_height` х `cell_width`.
+        """
+        return [[random.choice([0, 1]) if randomize else 0 for _ in range(self.cols)] for _ in
+                range(self.rows)]
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        """
+        Вернуть список соседних клеток для клетки `cell`.
+
+        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
+        то есть, во всех направлениях.
+
+        Parameters
+        ----------
+        cell : Cell
+            Клетка, для которой необходимо получить список соседей. Клетка
+            представлена кортежем, содержащим ее координаты на игровом поле.
+
+        Returns
+        ----------
+        out : Cells
+            Список соседних клеток.
+        """
+        x, y = cell
+        neighbours = []
+        for next_x in range(-1, 2):
+            for next_y in range(-1, 2):
+                if (next_x != 0 or next_y != 0) and \
+                        0 <= x + next_x < self.rows and \
+                        0 <= y + next_y < self.cols:
+                    neighbours.append(self.curr_generation[x + next_x][y + next_y])
+        return neighbours
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        """
+        Получить следующее поколение клеток.
+
+        Returns
+        ----------
+        out : Grid
+            Новое поколение клеток.
+        """
+        new_gen = self.create_grid(False)
+        for x in range(0, self.rows):
+            for y in range(0, self.cols):
+                neighbours = self.get_neighbours((x, y))
+                if self.curr_generation[x][y] and 2 <= sum(neighbours) <= 3:
+                    new_gen[x][y] = 1
+                elif not self.curr_generation[x][y] and sum(neighbours) == 3:
+                    new_gen[x][y] = 1
+        self.generations += 1
+        return new_gen
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        self.prev_generation = self.curr_generation
+        self.curr_generation = self.get_next_generation()
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        return self.generations <= self.max_generations
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.curr_generation != self.prev_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        file = open(filename)
+        grid = [list(map(int, line)) for line in file.readlines()]
+        file.close()
+
+        rows = len(grid)
+        cols = len(grid[0])
+        game_instance = GameOfLife((rows, cols), False)
+        game_instance.curr_generation = grid
+
+        return game_instance
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        file = open(filename, 'w')
+        for line in self.curr_generation:
+            print(''.join(map(str, line)), file=file, sep='')
+        file.close()
