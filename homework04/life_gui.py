2728284e346076d015b00b5a5c2c00635a07459e
LedviL <samrinis@mail.ru>
2025-12-14 20:59:42 +0300

    done life_gui.py

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -6,16 +6,58 @@ from ui import UI
 
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
+        self.cell_size = cell_size
+        self.rows, self.cols = life.rows, life.cols
+        self.width, self.height = self.cols * cell_size, self.rows * cell_size
+
+        self.speed = speed
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
         super().__init__(life)
 
     def draw_lines(self) -> None:
-        # Copy from previous assignment
-        pass
+        """ Отрисовать сетку """
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
-        pass
+        """
+        Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
+        """
+        for x in range(0, self.height, self.cell_size):
+            for y in range(0, self.width, self.cell_size):
+                cell_x = x // self.cell_size
+                cell_y = y // self.cell_size
+                pygame.draw.rect(
+                    self.screen,
+                    pygame.Color("green" if self.life.curr_generation[cell_x][cell_y] else "white"),
+                    (y + 1, x + 1, self.cell_size - 1, self.cell_size - 1)
+                )
 
     def run(self) -> None:
-        # Copy from previous assignment
-        pass
+        """ Запустить игру """
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+
+            self.draw_lines()
+            self.draw_grid()
+
+            # Выполнение одного шага игры (обновление состояния ячеек)
+            self.life.step()
+
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+l = GUI(GameOfLife((48, 64)), 10, 5)
+l.run()
